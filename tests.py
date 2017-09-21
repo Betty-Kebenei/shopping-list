5432bdf6dc3ae085729c8e8a79af70720a28b544
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-09-21 15:43:05 +0300

    Adding more tests

--- tests.py
+++ tests.py
@@ -10,14 +10,19 @@ class Test(unittest.TestCase):
 
     def test_reg(self):
         sampletest = app.test_client(self)
-        response = sampletest.post('/signup', data=dict(first_name="bety",
+        response = sampletest.post('/signup', data=list(first_name="bety",
                                                         last_name="keb",
                                                         username="bk",
                                                         email="kb@gmail.com",
                                                         password="123"),                                                        
                                    follow_redirects=True)
         self.assertTrue('Please' in response.data)
-
+    
+    def test_list_create(self):
+        sampletest = app.test_client(self)
+        response = sampletest.post('/shop_list', data=list(list_name="one"),                                                        
+                                   follow_redirects=True)
+        self.assertTrue('Welcome' in response.data)
 
 
 
