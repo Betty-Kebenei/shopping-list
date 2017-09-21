9424c25f872bc7a960e66e8aaf6d7fe21372ea78
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-09-21 15:35:33 +0300

    Adding logout, create_list functionalities

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 import random
 
 users=[]
+shopping_list=[]
+shopping_items=[]
 class User(object):
     
     def __init__(self,first_name,last_name,username,email,password):
@@ -11,31 +13,38 @@ class User(object):
         self.email=email
         self.password=password
 
-    def login(self):
-        if users.get(self.email):
-            if users[self.email] == self.password:
-                return "Logged in successfully"
-            return "Wrong Password"
-        return "You are not registered"
-  
 
-#class Shopping_list(object):
-   # def __init__(self,id_number,owner):
-        #self.id_number=id_number #number of the shopping_list
+class Shopping_list(object):
+    def __init__(self,listname):
+        self.id_number=random.randrange(1,1000,1) #number of the shopping_list
         #self.owner=owner    #owner of the shopping list
-    
-    #def create_shopping_list(self):
+        self.listname=listname
+
+    #def create_shopping_list(self,name):
+
+
+    def view_shopping_list(self):
+        return shopping_list
 
     #def update_shopping_list(self):
 
-    #def delete_shopping_list(self):
+    def delete_shopping_list(self,list_to_delete):
+        if list_to_delete in shopping_list:
+            shopping_list.remove(list_to_delete)
+            return "List deleted."
 
-#class Shopping_items(object):
-    #def __init__(self,list):
-        #self.list=list
+class Shopping_items(object):
+    def __init__(self,list):
+        self.list=list
        
     #def add_shopping_item(self):
 
-    #def update_shopping_update(self):
+    def view_shopping_items(self):
+        return shopping_items
 
-    #def delete_shopping_delete(self):    
+    #def update_shopping_item(self):
+
+    def delete_shopping_item(self):
+        if item_to_delete in Shopping_items:
+            shopping_items.remove(item_to_delete)
+            return "Item deleted."    
