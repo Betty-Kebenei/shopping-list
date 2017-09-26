d03f64ba87b54826574780c3879530684dbc6b89
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-09-26 16:39:06 +0300

    Made changes to the dashboard

--- app/models.py
+++ app/models.py
@@ -1,31 +1,30 @@
 import random
 
-users=[]
-shopping_list=[]
-shopping_items=[]
-class User(object):
-    
-    def __init__(self,first_name,last_name,username,email,password):
-        self.id_number=random.randrange(1,1000,1)
-        self.first_name=first_name
-        self.last_name=last_name
-        self.username=username
-        self.email=email
-        self.password=password
+users = []
+shopping_list = []
+shopping_items = []
+class User(object):   
+    def __init__(self, first_name, last_name, username, email, password):
+        self.id_number = random.randrange(1, 1000, 1)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.password = password
 
 
 class Shopping_list(object):
-    def __init__(self,listname):
-        self.list_id=random.randrange(1,1000,1) #number of the shopping_list
+    def __init__(self, listname):
+        self.list_id = random.randrange(1, 1000, 1) #number of the shopping_list
         #self.owner=owner    #owner of the shopping list
         self.listname=listname
 
 
 class Shopping_items(object):
-    def __init__(self,itemname,quantity,price):
-        self.itemname=itemname
-        self.quantity=quantity
-        self.price=price
+    def __init__(self, itemname, quantity, price):
+        self.itemname = itemname
+        self.quantity = quantity
+        self.price = price
        
     #def add_shopping_item(self):
 
--- app/views.py
+++ app/views.py
@@ -1,14 +1,12 @@
 # views.py
-import string
-from models import User,users,Shopping_list,shopping_list,Shopping_items,shopping_items
-from flask import render_template, redirect, request,url_for, flash
-
+from app.models import User, users, Shopping_list, shopping_list, Shopping_items, shopping_items
+from flask import render_template, redirect, request, url_for, flash
 from app import app
+s = ''
 
 @app.route('/')
 def dashboard():
     return render_template("dashboard.html")
-
 
 @app.route('/signup',methods=['POST', 'GET'])
 def signup():
@@ -19,13 +17,12 @@ def signup():
         email = request.form['email']
         password = request.form['psw']
 
-        user = User(first_name, last_name, user_name, email,password)
+        user = User(first_name, last_name, user_name, email, password)
         users.append(user)
-        
+     
         return redirect(url_for('signin'))
     return render_template("signup.html")
-
-    
+   
 @app.route('/login', methods=['POST', 'GET'])
 def signin():
     if request.method == 'POST':
@@ -34,11 +31,9 @@ def signin():
         for user in users:
             if email == user.email:
                 if password == user.password:
-                    
                     return redirect(url_for('dashboard'))
                 else:
                     flash('Wrong password!')
-
             else:
                 flash('User not found!')
     return render_template("signin.html")
@@ -47,28 +42,26 @@ def signin():
 def logout():
     return redirect(url_for('signin'))
 
-
 @app.route('/shop_list',methods=['POST', 'GET'])
 def shop_list():
     if request.method == 'POST':
-        listname = request.form['listname'].strip()
-        if len(listname) != 0:
+        listname = request.form['listname'].strip()   
+        if listname != s:
             s_list = Shopping_list(listname)
-            shopping_list.append(s_list)   
-        else:
-            raise TypeError('No empty strings allowed.')
-
-        return render_template("dashboard.html", shopping_list = shopping_list)
+            shopping_list.append(s_list) 
+        else:   
+            flash("No empty strings allowed.")
+        return render_template("dashboard.html", shopping_list=shopping_list)
     return render_template("dashboard.html")
 
-@app.route('/shop_list/<list_id>/del',methods=['DELETE'])
-def del_shop_list(list_id):
-    shop_list=listname.query.get(list_id)
-    if shop_list is None:
-        return jsonify(
-            status='error', message='shopping list with id {}'.format(list_id))
-    shopping_list.delete(shop_list)
-    return jsonify(status='ok')
+# @app.route('/shop_list/<list_id>/del',methods=['DELETE'])
+# def del_shop_list(list_id):
+#     list_to_del=listname.query.get(list_id)
+#     if list_to_del is None:
+#         return jsonify(
+#             status='error', message='shopping list with id {}'.format(list_id))
+#     shopping_list.remove(list_to_del)
+#     return jsonify(status='ok')
 
     # for items in shopping_items:
     #     name = request.form['item.listname']
@@ -78,29 +71,27 @@ def del_shop_list(list_id):
     #     return redirect(url_for('dashboard'))
     # return render_template("signin.html")
 
-@app.route('/view_lists',methods=['POST', 'GET'])
+@app.route('/view_lists', methods=['POST', 'GET'])
 def view_listss():
     if request.method == 'POST':
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('shop_list'))
     return render_template("shoppingitems.html")
 
-@app.route('/view_items',methods=['POST', 'GET'])
+@app.route('/view_items', methods=['POST', 'GET'])
 def view_items():
     if request.method == 'POST':
         return redirect(url_for('shop_item'))
     return render_template("dashboard.html")
 
-@app.route('/shop_item',methods=['POST', 'GET'])
+@app.route('/shop_item', methods=['POST', 'GET'])
 def shop_item():
     if request.method == 'POST':
         itemname = request.form['itemname']
-        quantity= request.form['quantity']
+        quantity = request.form['quantity']
         price = request.form['price']
 
-        item = Shopping_items(itemname,quantity,price)
+        item = Shopping_items(itemname, quantity, price)
         shopping_items.append(item)
 
-        return render_template("shoppingitems.html", shopping_items = shopping_items)
+        return render_template("shoppingitems.html", shopping_items=shopping_items)
     return render_template("shoppingitems.html")
-    
-
