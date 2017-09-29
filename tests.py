4fc3f220201bedea8f7634f308ce7158582a566c
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-09-29 08:35:47 +0300

    Add tests

--- tests.py
+++ tests.py
@@ -18,16 +18,18 @@ class UserTestCase(unittest.TestCase):
 
     def test_signup_successful(self):
         res = app.test_client(self)
-        response = res.post('/signup', users=(firstname="firstname", lastname="lastname", username="username", email="email", password="password", con_password="con_password") follow_redirects=True)
-        self.assertTrue('email' in response.data)
+        response = res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
+                                                    email="hey@gmail.com", password="123"), 
+                                                    follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_login_passes(self):
         """Ensuring that login passes as expected"""
 
         res = app.test_client(self)
-        response = res.post('/login', data(username="email", password="password"),
-                                   follow_redirects=True)
-        self.assertTrue('account' in response.data)
+        response = res.post('/login', data = dict(username="hey@gmail.com", password="123"),
+                                    follow_redirects=True)
+        self.assertTrue('Welcome' in response.data)
 
 class ShoppinglistsTestCase(unittest.TestCase):
     """This class represents the shopping list test cases"""
@@ -40,15 +42,54 @@ class ShoppinglistsTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_shoppinglist_creation(self):
-        """Test creation of a shoppinglist (POST request)"""
+        """Test creation of a shoppinglist"""
 
         res = app.test_client(self)
-        response = res.post('/shop_list/', data=self.shop_list)
-        self.assertEqual(res.status_code, 201)
+        res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
+                                                    email="hey@gmail.com", password="123"), 
+                                                    follow_redirects=True)
+        res.post('/login', data = dict(username="hey@gmail.com", password="123"),
+                                    follow_redirects=True)
+        response = res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_shoppinglist_deletion(self):
+        """Test deletion of a shoppinglist """
+
+        res = app.test_client(self)
+        res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
+                                                    email="hey@gmail.com", password="123"), 
+                                                    follow_redirects=True)
+        res.post('/login', data = dict(username="hey@gmail.com", password="123"),
+                                    follow_redirects=True)
+        res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
+        response = res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 
 class ShoppingItemsTestCase(unittest.TestCase):  
     """This class represents the shopping items test cases"""  
+
+    def test_shoppingitems_page_loads(self):
+        """Test if the page used to add shopping items loads """
+
+        res = app.test_client(self)
+        response = res.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_shoppingitems_creation(self):
+        """Test creation of an item in the shopping list"""
+
+        res = app.test_client(self)
+        res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
+                                                    email="hey@gmail.com", password="123"), 
+                                                    follow_redirects=True)
+        res.post('/login', data = dict(username="hey@gmail.com", password="123"),
+                                    follow_redirects=True)
+        res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
+        response = res.post('/shop_item', data=dict(itemname='itemname', quantity='quantity', price='price'), 
+                                                        follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
