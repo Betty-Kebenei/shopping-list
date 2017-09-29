5c9637d177154780b614a9cbf73a59856c845ba5
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-09-29 08:54:36 +0300

    Add correct PEP8 style

--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ class UserTestCase(unittest.TestCase):
     """This class represents the user test cases"""
 
     def create_app(self):
+        """Creating an instance"""
+
         app = Flask(__name__)
         app.config['TESTING'] = True
 
@@ -17,18 +19,22 @@ class UserTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_signup_successful(self):
+        """Test if signup is successful """
+
         res = app.test_client(self)
-        response = res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
-                                                    email="hey@gmail.com", password="123"), 
-                                                    follow_redirects=True)
+        response = res.post('/signup', data=dict(firstname="firstname",
+                                                 lastname="lastname",
+                                                 username="username",
+                                                 email="hey@gmail.com",
+                                                 password="123"), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
     def test_login_passes(self):
         """Ensuring that login passes as expected"""
 
         res = app.test_client(self)
-        response = res.post('/login', data = dict(username="hey@gmail.com", password="123"),
-                                    follow_redirects=True)
+        response = res.post('/login', data=dict(username="hey@gmail.com",
+                                                password="123"), follow_redirects=True)
         self.assertTrue('Welcome' in response.data)
 
 class ShoppinglistsTestCase(unittest.TestCase):
@@ -45,11 +51,13 @@ class ShoppinglistsTestCase(unittest.TestCase):
         """Test creation of a shoppinglist"""
 
         res = app.test_client(self)
-        res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
-                                                    email="hey@gmail.com", password="123"), 
-                                                    follow_redirects=True)
-        res.post('/login', data = dict(username="hey@gmail.com", password="123"),
-                                    follow_redirects=True)
+        res.post('/signup', data=dict(firstname="firstname",
+                                      lastname="lastname",
+                                      username="username",
+                                      email="hey@gmail.com",
+                                      password="123"), follow_redirects=True)
+        res.post('/login', data=dict(username="hey@gmail.com",
+                                     password="123"), follow_redirects=True)
         response = res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
@@ -57,19 +65,21 @@ class ShoppinglistsTestCase(unittest.TestCase):
         """Test deletion of a shoppinglist """
 
         res = app.test_client(self)
-        res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
-                                                    email="hey@gmail.com", password="123"), 
-                                                    follow_redirects=True)
-        res.post('/login', data = dict(username="hey@gmail.com", password="123"),
-                                    follow_redirects=True)
+        res.post('/signup', data=dict(firstname="firstname",
+                                      lastname="lastname",
+                                      username="username",
+                                      email="hey@gmail.com",
+                                      password="123"), follow_redirects=True)
+        res.post('/login', data=dict(username="hey@gmail.com",
+                                     password="123"), follow_redirects=True)
         res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
         response = res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 
 class ShoppingItemsTestCase(unittest.TestCase):  
-    """This class represents the shopping items test cases"""  
-
+    """This class represents the shopping items test cases"""
+    
     def test_shoppingitems_page_loads(self):
         """Test if the page used to add shopping items loads """
 
@@ -81,14 +91,18 @@ class ShoppingItemsTestCase(unittest.TestCase):
         """Test creation of an item in the shopping list"""
 
         res = app.test_client(self)
-        res.post('/signup', data=dict(firstname="firstname", lastname="lastname", username="username",
-                                                    email="hey@gmail.com", password="123"), 
-                                                    follow_redirects=True)
-        res.post('/login', data = dict(username="hey@gmail.com", password="123"),
-                                    follow_redirects=True)
+        res.post(
+            '/signup', data=dict(firstname="firstname",
+                                 lastname="lastname",
+                                 username="username",
+                                 email="hey@gmail.com",
+                                 password="123"), follow_redirects=True)
+        res.post('/login', data=dict(
+            username="hey@gmail.com", password="123"), follow_redirects=True)
         res.post('/shop_list', data=dict(listname="books"), follow_redirects=True)
-        response = res.post('/shop_item', data=dict(itemname='itemname', quantity='quantity', price='price'), 
-                                                        follow_redirects=True)
+        response = res.post(
+            '/shop_item', data=dict(
+                itemname='itemname', quantity='quantity', price='price'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
