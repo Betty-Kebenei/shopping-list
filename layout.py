4a32015dc643e92ca435450fa7daab8c5accfcb3
gr1d99 <gideonkimutai9@gmail.com>
2017-10-07 17:02:15 +0300

    [CHORE]Add tasks needed to be done

---
+++ layout.py
@@ -0,0 +1,14 @@
+"""
+    class ShoppingList
+        id -> unique
+        created_by -> user-email
+        items = [] -> list
+
+    class ShoppingItem
+        item_name -> unique
+        quantity
+        price
+
+    Todo:
+        - find a way to pass shopping list id when adding shopping items.
+"""
